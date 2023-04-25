d50f5c8c98149147f147436718601364a2ae5109
deshan68 <arundeshan@gmail.com>
2023-04-25 18:43:17 +0530

    conect firbease

--- src/config/firebase-config.jsx
+++ src/config/firebase-config.jsx
@@ -2,10 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { GOOGLE_FIREBASE_API_KEY } from "../../environment";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD81gI4AD72VQKzGyMq4j_6o-my9RcPb-I",
+  apiKey: GOOGLE_FIREBASE_API_KEY,
   authDomain: "my-portfolio-db-4f84e.firebaseapp.com",
   projectId: "my-portfolio-db-4f84e",
   storageBucket: "my-portfolio-db-4f84e.appspot.com",
